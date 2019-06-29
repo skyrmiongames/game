b6dd89f5241783356a15703139421c960f7b2ed1
Duncan Gibson <legowerewolf@gmail.com>
2019-06-29 16:26:43 -0700

    subpath builds

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,12 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const path = require("path");
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
+	mode: "development",
+	devtool: "inline-source-map",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "build", "dev"),
+	},
 });
