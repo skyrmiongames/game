5c2d9003097999a9a64aaa8a84072966d6baff24
Duncan Gibson <legowerewolf@gmail.com>
2019-08-02 18:59:29 -0700

    Resource loading demo

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
-import { Application } from "pixi.js";
+import { Application, IResourceDictionary, Loader, Sprite } from "pixi.js";
 import "./main.css";
 
-const app = new Application();
+const app = new Application({ resizeTo: window, resolution: 1 }); // Create the Pixi base
+document.body.appendChild(app.view); // Inject it
 
-document.body.appendChild(app.view);
+Loader.shared
+	.add([
+		require("../res/sprite/core.png"), // must use webpack require
+	])
+	.on("start", () => {
+		console.log("Loading...");
+	})
+	.on("progress", (loader, resource) => {
+		console.log(`Loaded ${resource.name} (${loader.progress}%)`);
+	})
+	.on("complete", main)
+	.load();
 
-app.renderer.autoResize = true;
+function main(loader: Loader, obj: IResourceDictionary) {
+	console.log("All resources loaded.");
+	console.log(obj);
+
+	let core = new Sprite(obj[require("../res/sprite/core.png")].texture);
+	core.alpha = 0;
+
+	app.stage.addChild(core);
+}
--- webpack.common.js
+++ webpack.common.js
@@ -16,7 +16,10 @@ module.exports = {
 			},
 			{
 				test: /\.(png|svg|jpg|gif)$/,
-				use: ["file-loader"],
+				use: {
+					loader: "file-loader",
+					options: { name: "[name]-[hash].[ext]" },
+				},
 			},
 		],
 	},
